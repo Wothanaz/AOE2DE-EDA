548ca745619ea25543d07f9b074bcd33051e0491
Juan Santiago Gutiérrez Estrada <jsgutierreze@gmail.com>
2024-05-13 12:52:15 -0500

    Commit 1
    Beginning of the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An exploratory Data Analysis of Age of Empires 2 DE',
+    author='Santiago Gutierrez',
+    license='MIT',
+)
